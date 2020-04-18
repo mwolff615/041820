4fd924aba7997340413c79620c65da80699ff309
mwolff615 <42stuff42@gmail.com>
2020-04-18 15:13:49 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#day 1 
+
+#pre-course survey
+    #https://docs.google.com/forms/d/1sLm3W-IT1SON14nHPcDX5AggjQDL6GF6a0khNXpbvMc/viewform?edit_requested=true
+
+#repository 
+
+#print statements 
+
+#printing numbers 
+
+#escape characters 
+
+#definitions 
+
+#input-process-output 
